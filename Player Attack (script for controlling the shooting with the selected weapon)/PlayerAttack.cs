463ede32a01d0bd45e05ec3621aef876a5f1f567
Khurram <96042023+khurramgamedevelopment@users.noreply.github.com>
2022-03-28 07:52:16 +0500

    Update PlayerAttack.cs

--- Player Attack (script for controlling the shooting with the selected weapon)/PlayerAttack.cs
+++ Player Attack (script for controlling the shooting with the selected weapon)/PlayerAttack.cs
@@ -9,10 +9,26 @@ public class PlayerAttack : MonoBehaviour {
     public float fireRate = 15f;
     private float nextTimeToFire;
     public float damage = 20f;
+    private Animator zoomCameraAnim;
+    private bool zoomed;
+
+    private Camera mainCam;
+
+    private GameObject crosshair;
+
+    private bool is_Aiming;
+
+
 
     void Awake() {
 
         weapon_Manager = GetComponent<WeaponManager>();
+         zoomCameraAnim = transform.Find(Tags.LOOK_ROOT)
+                                  .transform.Find(Tags.ZOOM_CAMERA).GetComponent<Animator>();
+
+        crosshair = GameObject.FindWithTag(Tags.CROSSHAIR);
+
+        mainCam = Camera.main;
     }
 
     // Use this for initialization
@@ -23,6 +39,7 @@ public class PlayerAttack : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         WeaponShoot();
+        ZoomInAndOut();
         
     }
 
@@ -60,39 +77,76 @@ public class PlayerAttack : MonoBehaviour {
                 } else {
 
                     // we have an arrow or spear
+                     if(is_Aiming) {
+
+                        weapon_Manager.GetCurrentSelectedWeapon().ShootAnimation();
+
+                        if(weapon_Manager.GetCurrentSelectedWeapon().bulletType
+                           == WeaponBulletType.ARROW) {
+
+                            // throw arrow
+                            ThrowArrowOrSpear(true);
+
+                        } else if(weapon_Manager.GetCurrentSelectedWeapon().bulletType
+                                  == WeaponBulletType.SPEAR) {
+
+                            // throw spear
+                            ThrowArrowOrSpear(false);
+
+                        }
+
+                    }
+
                 }
             }
                     
 
-    }                
+    } 
+     void ZoomInAndOut() {
+
+        // we are going to aim with our camera on the weapon
+        if(weapon_Manager.GetCurrentSelectedWeapon().weapon_Aim == WeaponAim.AIM) {
+
+            // if we press and hold right mouse button
+            if(Input.GetMouseButtonDown(1)) {
+
+                zoomCameraAnim.Play(AnimationTags.ZOOM_IN_ANIM);
+
+                crosshair.SetActive(false);
+            }
+
+            // when we release the right mouse button click
+            if (Input.GetMouseButtonUp(1)) {
+
+                zoomCameraAnim.Play(AnimationTags.ZOOM_OUT_ANIM);
+
+                crosshair.SetActive(true);
+            }
+
+        } // if we need to zoom the weapon
+
+        if(weapon_Manager.GetCurrentSelectedWeapon().weapon_Aim == WeaponAim.SELF_AIM) {
+
+            if(Input.GetMouseButtonDown(1)) {
+
+                weapon_Manager.GetCurrentSelectedWeapon().Aim(true);
+
+                is_Aiming = true;
+
+            }
+
+            if (Input.GetMouseButtonUp(1)) {
+
+                weapon_Manager.GetCurrentSelectedWeapon().Aim(false);
+
+                is_Aiming = false;
+
+            }
+
+        } // weapon self aim
+
+    } // zoom in and out
+
+
 }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
